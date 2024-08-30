5865a6cb59a0a882c5894a148afe769a470966c9
jsxlei <jsxlei@gmail.com>
2024-08-30 15:25:30 +0000

    rename motif

--- chrombpnet/scripts/rename_motif.py
+++ chrombpnet/scripts/rename_motif.py
@@ -7,6 +7,13 @@ parser.add_argument('--motif_html', help='Modisco h5 file')
 parser.add_argument('--bed', help='Hits caller bed file')
 args = parser.parse_args()
 
+def strip_motif_name(s):
+    try: 
+        s.split('_')[0].split('.mouse')[0].upper()
+    except:
+        print(s)
+        return 'NANANANANANAN'
+    return s.split('_')[0].split('.mouse')[0].upper()
 
 with open(args.motif_html, "r") as file:
     modisco_report = BeautifulSoup(file, "html.parser")
@@ -17,5 +24,5 @@ mapping_dict = pd.Series(df['match0'].values, df['pattern'].values).to_dict()
 
 hits = pd.read_csv(args.bed, sep="\t", header=None)
 hits.columns = ["chrom", "start", "end", "pattern", "score", "strand"]
-hits['pattern'] = hits['pattern'].map(mapping_dict)
+hits['pattern'] = hits['pattern'].map(mapping_dict).apply(strip_motif_name)
 hits.to_csv(args.bed, sep="\t", header=None, index=None) #, compression='bgzip')
